78a4e548d2e873f70148fd2e1202822458262352
joshua kilaha <jokilaha191@gmail.com>
2020-04-01 18:44:11 +0300

    Fetching JSON from IOS.

---
+++ IOS/Travel Log/Model/LogEntry.swift
@@ -0,0 +1,23 @@
+//
+//  LogEntry.swift
+//  Travel Log
+//
+//  Created by Kilz on 31/03/2020.
+//  Copyright © 2020 Kilz. All rights reserved.
+//
+
+import Foundation
+
+class LogEntry: Decodable {
+    
+    let _id: String
+    let title: String
+    let comments: String
+    let rating: Int
+    let description: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    let visitDate: String
+
+}
---
+++ IOS/Travel Log/View/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Travel Log
+//
+//  Created by Kilz on 31/03/2020.
+//  Copyright © 2020 Kilz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+    
+    
+
+
+}
+
