651d3b8064cd7bb6f9ea26d11c7b972ec6483e7f
joshua kilaha <jokilaha191@gmail.com>
2020-05-16 18:32:01 +0300

    adding DELETE requset from server

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,8 +1,30 @@
+// import LogEntries from "../../../client/Vue/src/components/LogEntries";
+
 const { Router } =   require('express');
 const router = Router();
 
 const LogEntry =  require('../modules/modelstuct');
 
+
+//POST Request
+router.post('/', async function (req, res, next) {
+    try{
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+        console.log(req.body);
+
+    } catch (error) {
+        console.error(error.name);
+        if (error.name === 'VadilationError') {
+            res.status(422);
+        }
+        next(error);
+    }
+
+});
+
+//GET Request
 router.get('/', async function (req, res) {
    try {
        const showEntries = await LogEntry.find();
@@ -13,18 +35,17 @@ router.get('/', async function (req, res) {
    }
 });
 
-router.post('/', async function (req, res, next) {
-    try{
-        const logEntry = new LogEntry(req.body);
-        const createdEntry = await logEntry.save();
-        res.json(createdEntry);
-        console.log(req.body);
-    } catch (error) {
-        console.error(error.name);
-        if (error.name === 'VadilationError') {
-            res.status(422);
-        }
-        next(error);
+//DELETE Request
+router.delete('/:id', async function (req, res) {
+    try {
+        const destination = await  LogEntry.deleteOne({
+            _id: req.params.id
+        })
+            .exec();
+        res.send(destination);
+    }
+    catch (error) {
+        res.status(500).send(error);
     }
 
 });
